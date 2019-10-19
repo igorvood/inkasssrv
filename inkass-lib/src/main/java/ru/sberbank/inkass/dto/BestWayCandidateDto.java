18a7843b99578d5d9b81840acbe27fb0390bcbf2
vinni <balitsky_igor@list.ru>
2019-10-19 17:29:09 +0300

    way graph

--- inkass-lib/src/main/java/ru/sberbank/inkass/dto/BestWayCandidateDto.java
+++ inkass-lib/src/main/java/ru/sberbank/inkass/dto/BestWayCandidateDto.java
@@ -2,14 +2,11 @@ package ru.sberbank.inkass.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.apache.commons.lang3.tuple.MutablePair;
-
-import java.util.List;
 
 @Data
 @AllArgsConstructor
 public class BestWayCandidateDto {
-    transient private final List<MutablePair<PointDto, PointDto>> wayPair;
+    //    transient private final List<MutablePair<PointDto, PointDto>> wayPair;
     private final double totalTime;
     private final double totalMoney;
 
