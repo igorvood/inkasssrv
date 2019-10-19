2de708979bb9ed2ae6e4f905a13f4d7b23869dae
vinni <balitsky_igor@list.ru>
2019-10-19 21:08:49 +0300

    way graph

--- way-graph-srv/src/main/java/ru/sberbank/infrastructure/PropertiesWithJavaConfig.java
+++ way-graph-srv/src/main/java/ru/sberbank/infrastructure/PropertiesWithJavaConfig.java
@@ -39,6 +39,8 @@ public class PropertiesWithJavaConfig {
                 , maxTimeInPoint
                 , maxTimeInWay
                 , maxSumInPoint * maxMoneyInAntMultiplier
-                , workingDayLength);
+                , workingDayLength
+                , speedTranspirationPheromone
+        );
     }
 }
