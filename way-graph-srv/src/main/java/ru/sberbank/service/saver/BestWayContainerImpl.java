e0981d0624d7c02aec0f04c164a475964da78cc4
vinni <balitsky_igor@list.ru>
2019-10-19 19:48:54 +0300

    way graph

--- way-graph-srv/src/main/java/ru/sberbank/service/saver/BestWayContainerImpl.java
+++ way-graph-srv/src/main/java/ru/sberbank/service/saver/BestWayContainerImpl.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Service;
 import ru.sberbank.inkass.dto.BestWayCandidateDto;
 
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.logging.Logger;
 
 @Service
 public class BestWayContainerImpl implements BestWayContainer {
-
+    private static final Logger logger = Logger.getLogger(BestWayContainerImpl.class.getName());
     private CopyOnWriteArrayList<BestWayCandidateDto> bestWayCandidateDtos;
 
     public BestWayContainerImpl() {
@@ -16,6 +17,7 @@ public class BestWayContainerImpl implements BestWayContainer {
 
     @Override
     public void reset() {
+        logger.info("Reset best way");
         bestWayCandidateDtos = new CopyOnWriteArrayList<>();
     }
 
