b38f51d200da8ab185cb71b8c3f0178ab4f7e65b
vood <balitsky_igor@list.ru>
2019-10-19 16:45:26 +0300

    исправил Dto

--- inkass-lib/src/main/java/ru/sberbank/inkass/dto/BestWayCandidateDto.java
+++ inkass-lib/src/main/java/ru/sberbank/inkass/dto/BestWayCandidateDto.java
@@ -2,14 +2,14 @@ package ru.sberbank.inkass.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 public class BestWayCandidateDto {
-    private final List<Pair<PointDto, PointDto>> wayPair;
+    private final List<ImmutablePair<PointDto, PointDto>> wayPair;
     private final double totalTime;
     private final double totalMoney;
 
